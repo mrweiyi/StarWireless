7d730fa2e8513b71ba4757854b76e90a3c4d7571
Yi Wei <wei118@usc.edu>
2018-03-18 22:50:50 -0700

    v1.3

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -23,19 +23,30 @@ class DetailScreen extends React.Component {
           Start By Selecting Your Device That Needs Repairing 
         </Text>
 
+      <View style={styles.introduce2}>
+        <Text style={styles.introduceText2}>
+          iPhone
+        </Text>
+        <Text style={styles.introduceText2}>
+          iPhone 6S
+        </Text>
+        <Text style={styles.introduceText2}>
+          Color : white
+        </Text>
+        <Text style={styles.introduceText2}>
+          Broken LCD Screen
+        </Text>
+      </View>
+
         <Button
 				  title= {'Submit'} 
-				  onPress={() => {Communications.text('2134216770','SWY I Love U'); }}
+				  onPress={() => {Communications.text('2134216770','StarWireless!!'); }}
 				  icon={{name: 'av-timer'}}
 				  buttonStyle={{height: 50, width: 250, backgroundColor: 'rgba(199, 43, 98, 1)', borderWidth: 2, borderColor: 'white', borderRadius: 30}}
 				  textStyle={{fontWeight: 'bold'}}
 				/>	
 
 
-
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
@@ -55,8 +66,20 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 20,
     textAlign: 'center',
-
   },
+
+  introduce2: {
+    marginTop: SCREEN_HEIGHT*0.11,
+    marginLeft: SCREEN_WIDTH*0.1,
+    marginRight:SCREEN_WIDTH*0.1,
+    marginBottom: SCREEN_HEIGHT*0.11,
+  },
+
+  introduceText2: {
+    fontSize: 17,
+    textAlign: 'center',
+  },
+
 });
 
 
